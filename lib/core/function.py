dfec1d144e754c66b996cecf3826b6bac2481380
leo <851268460@qq.com>
2020-12-30 10:51:49 +0800

    fix DDP bugs

--- lib/core/function.py
+++ lib/core/function.py
@@ -22,14 +22,16 @@ def train(config, train_loader, model, criterion, optimizer, epoch,
     None
 
     """
-    batch_time = AverageMeter()
-    data_time = AverageMeter()
-    losses = AverageMeter()
+    if rank in [-1, 0]:
+        batch_time = AverageMeter()
+        data_time = AverageMeter()
+        losses = AverageMeter()
     # switch to train mode
     model.train()
     start = time.time()
     for i, (input, target) in enumerate(train_loader):
-        data_time.update(time.time() - start)
+        if rank in [-1, 0]:
+            data_time.update(time.time() - start)
         if not config.DEBUG:
             input = input.to(device, non_blocking=True)
             assign_target = []
@@ -44,32 +46,34 @@ def train(config, train_loader, model, criterion, optimizer, epoch,
         optimizer.zero_grad()
         total_loss.backward()
         optimizer.step()
-        # measure accuracy and record loss
-        losses.update(total_loss.item(), input.size(0))
 
-        # _, avg_acc, cnt, pred = accuracy(output.detach().cpu().numpy(),
-        #                                  target.detach().cpu().numpy())
-        # acc.update(avg_acc, cnt)
+        if rank in [-1, 0]:
+            # measure accuracy and record loss
+            losses.update(total_loss.item(), input.size(0))
 
-        # measure elapsed time
-        batch_time.update(time.time() - start)
-        end = time.time()
-        if i % config.PRINT_FREQ == 0:
-            msg = 'Epoch: [{0}][{1}/{2}]\t' \
-                  'Time {batch_time.val:.3f}s ({batch_time.avg:.3f}s)\t' \
-                  'Speed {speed:.1f} samples/s\t' \
-                  'Data {data_time.val:.3f}s ({data_time.avg:.3f}s)\t' \
-                  'Loss {loss.val:.5f} ({loss.avg:.5f})'.format(
-                      epoch, i, len(train_loader), batch_time=batch_time,
-                      speed=input.size(0)/batch_time.val,
-                      data_time=data_time, loss=losses)
-            logger.info(msg)
+            # _, avg_acc, cnt, pred = accuracy(output.detach().cpu().numpy(),
+            #                                  target.detach().cpu().numpy())
+            # acc.update(avg_acc, cnt)
 
-            writer = writer_dict['writer']
-            global_steps = writer_dict['train_global_steps']
-            writer.add_scalar('train_loss', losses.val, global_steps)
-            # writer.add_scalar('train_acc', acc.val, global_steps)
-            writer_dict['train_global_steps'] = global_steps + 1
+            # measure elapsed time
+            batch_time.update(time.time() - start)
+            end = time.time()
+            if i % config.PRINT_FREQ == 0:
+                msg = 'Epoch: [{0}][{1}/{2}]\t' \
+                      'Time {batch_time.val:.3f}s ({batch_time.avg:.3f}s)\t' \
+                      'Speed {speed:.1f} samples/s\t' \
+                      'Data {data_time.val:.3f}s ({data_time.avg:.3f}s)\t' \
+                      'Loss {loss.val:.5f} ({loss.avg:.5f})'.format(
+                          epoch, i, len(train_loader), batch_time=batch_time,
+                          speed=input.size(0)/batch_time.val,
+                          data_time=data_time, loss=losses)
+                logger.info(msg)
+
+                writer = writer_dict['writer']
+                global_steps = writer_dict['train_global_steps']
+                writer.add_scalar('train_loss', losses.val, global_steps)
+                # writer.add_scalar('train_acc', acc.val, global_steps)
+                writer_dict['train_global_steps'] = global_steps + 1
 
 
 def validate(config, val_loader, val_dataset, model, criterion, output_dir,
